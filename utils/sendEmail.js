81cc21f295dd2e390fdee856a96e65507574f4a3
VinayKashyap18 <119151715+VinayKashyap18@users.noreply.github.com>
2023-11-14 20:46:51 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,7 +10,7 @@ dotenv.config();
 const { AUTH_EMAIL, AUTH_PASSWORD, APP_URL } = process.env;
 
 let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp-mail.outlook.com",
     auth: {
         user: AUTH_EMAIL,
         pass: AUTH_PASSWORD,
